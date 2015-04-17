e27635cecee971769282c5836b854f7449809d8a
kyllyang <kyllyang@gmail.com>
2015-04-17 13:00:06 +0800

    设计页面需要展示的内容, 以及简单规则

--- standard/web/myserver/index.js
+++ standard/web/myserver/index.js
@@ -56,11 +56,15 @@ Ext.onReady(function() {
 	});
 
 	Ext.create('Base.ux.Notification', {
-		title: '地图设置',
-		html: 'Map, View, Source, Layer 配置， 专题配置'
+		title: '信息提示栏',
+		html: '登录用户， 当前应用（下拉）， 当前地图专题（下拉）， 最小化的业务窗口列表（下拉复选框， 选中显示， 不选中最小化）， 退出按钮'
 	}).show();
 	Ext.create('Base.ux.Notification', {
-		title: '应用配置',
+		title: '地图维护',
+		html: 'Map, View, Source, Layer 配置， 专题配置， 专题权限（精确到部门， 职位， 用户, 角色）'
+	}).show();
+	Ext.create('Base.ux.Notification', {
+		title: '应用维护',
 		html: '应用与地图专题关联， 应用与模块关联， 模块与功能关联， 功能与菜单关联， 业务功能菜单'
 	}).show();
 	Ext.create('Base.ux.Notification', {
@@ -68,7 +72,15 @@ Ext.onReady(function() {
 		html: '组织机构（部门管理， 职位管理， 用户管理， 角色分配）， 角色管理（角色建立， 与功能关联）， 数据字典， 系统常量配置'
 	}).show();
 	Ext.create('Base.ux.Notification', {
-		title: '业务功能菜单',
+		title: '业务展示',
 		html: '业务功能1， 业务功能2， 业务功能3'
 	}).show();
+
+	// 上面的每一个通知框， 都叫做“功能区域”
+	// 用户登录， 根据权限， 来展示拥有的功能区域。 功能区域内的功能可以有多种表现（按钮， 菜单）
+	// 登录后默认显示“信息提示栏”， “当前应用”的“业务展示”
+	// 应用-专题-模块-功能 （带有地图功能） 一个应用包含多个专题， 专题包含多个模块， 模块包含多个功能
+	// 应用-模块-功能 （不带有地图功能）一个应用包含多个模块， 模块包含多个功能
+	// 切换专题时， 可以选择是否切换业务功能
+	// 功能菜单可以将模块配置为通知框， 通知框内树形菜单， 手风琴菜单
 });
