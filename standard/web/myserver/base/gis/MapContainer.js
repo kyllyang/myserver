fb13641d3f4e0fea7b20d88a7ef2af2b252f5af0
Kyll Yang <kyllyang@gmail.com>
2015-05-26 19:56:36 +0800

    修改地图容器 DIV 标签 ID 生成方式

--- standard/web/myserver/base/gis/MapContainer.js
+++ standard/web/myserver/base/gis/MapContainer.js
@@ -1,12 +1,14 @@
 Ext.define('Base.gis.MapContainer', {
 	extend: 'Ext.container.Container',
 
-	itemId: 'mapContainer',
-	html: '<div id="mapDiv" style="width: 100%; height: 100%"></div>',
-
+	naoDivId: 'mapDiv_' + Ext.data.IdGenerator.get('uuid').generate().replace(/-/g, ''),
 	map: null,
 
 	initComponent: function() {
+		Ext.apply(this, {
+			itemId: 'mapContainer',
+			html: '<div id="' + this.naoDivId + '" style="width: 100%; height: 100%"></div>'
+		});
 		this.callParent();
 	},
 	getMap: function() {
@@ -14,14 +16,14 @@ Ext.define('Base.gis.MapContainer', {
 	},
 	loadMap: function(moduleId, thematicId) {
 		this.map = new ol.Map({
+			target: this.naoDivId,
 			layers: [
 				new ol.layer.Tile({source: new ol.source.OSM()})
 			],
 			view: new ol.View({
 				center: [0, 0],
 				zoom: 2
-			}),
-			target: 'mapDiv'
+			})
 		});
 	}
 });
